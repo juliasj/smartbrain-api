0491918e367a9d763740bd82a6d5b86474377752
Julia Sobrado <20336015+juliasj@users.noreply.github.com>
2018-11-11 17:17:23 +0100

    changes

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ const app = express();
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
+        host: '127.0.0.1',
+        user: '',
+        password: '',
+        database: 'smart-brain'
     }
 });
 
